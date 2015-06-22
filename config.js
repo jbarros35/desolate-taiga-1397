77510581c5ee5ff00f7d2915f63a776b081343d1
jbarros35 <jlopesde@ZCO-G8DWNX1.usersad.everis.int>
2015-06-22 10:33:36 -0300

    changed port database connection

--- config.js
+++ config.js
@@ -2,14 +2,15 @@ var url     = require('url'),
    dbUrl   = url.parse(process.env.DATABASE_URL),
    authArr = dbUrl.auth.split(':');
 var dbOptions = {};
-        dbOptions.name          = dbUrl.path.substring(1);
-        dbOptions.user          = authArr[0];
-        dbOptions.pass          = authArr[1];
-        dbOptions.host          = dbUrl.host.split(':')[0];
-        dbOptions.port          = dbUrl.host.split(':')[1];
-        dbOptions.dialect       = 'postgres';
-        dbOptions.protocol      = 'postgres';
 
+dbOptions.name          = dbUrl.path.substring(1);
+dbOptions.user          = authArr[0];
+dbOptions.pass          = authArr[1];
+dbOptions.host          = dbUrl.host.split(':')[0];
+dbOptions.port          = dbUrl.host.split(':')[1];
+dbOptions.dialect       = 'postgres';
+dbOptions.protocol      = 'postgres';
+console.log(dbUrl.host.split(':'));
 var config = {
 
   database: {
