de3ac9413cbd5d252dba1c961eac0a3715b0041a
Jose Carlos Barros <josecarlos.barros@gmail.com>
2015-07-14 23:17:27 -0400

    changed directories structure, changes in ngstorage

--- start.bat
+++ start.bat
@@ -5,10 +5,10 @@ set Path=%NodePackagesPath%\node_modules\.bin;%PATH%
 set Path=%NodePackagesPath%;%PATH%
 
 set NODE_PATH=%NodePackagesPath%\node_modules;%NODE_PATH%
-set NODE_ENV=production
+set NODE_ENV=development
 set tokenKey=mykeysign123456
 set DATABASE_URL=postgres://node:node@127.0.0.1:5432/business
-rem set BATCH=true
+set BATCH=true
 echo Environment variables are successfully added.
 echo. 
 echo. 
